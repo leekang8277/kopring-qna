0443c319782a1b0c6a8c0841e4fcecc745c8e12b
leekang8277 <ggomyi534@gmail.com>
2021-11-04 06:46:05 +0900

    git init

---
+++ src/main/kotlin/com/leekang/kopringqna/KopringQnaApplication.kt
@@ -0,0 +1,11 @@
+package com.leekang.kopringqna
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringQnaApplication
+
+fun main(args: Array<String>) {
+    runApplication<KopringQnaApplication>(*args)
+}
---
+++ src/test/kotlin/com/leekang/kopringqna/KopringQnaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.leekang.kopringqna
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KopringQnaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
